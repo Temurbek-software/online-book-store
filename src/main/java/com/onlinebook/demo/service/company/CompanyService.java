d1c91923426fff549cefa8bf6ca73a41dcd9cf20
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-08-06 00:24:59 +0500

    Create CompanyService.java

---
+++ src/main/java/com/onlinebook/demo/service/company/CompanyService.java
@@ -0,0 +1,15 @@
+package com.onlinebook.demo.service.company;
+
+import com.onlinebook.demo.entity.Company;
+import com.onlinebook.demo.payload.ApiResult;
+import com.onlinebook.demo.payload.CompanyDTO;
+
+import java.util.List;
+
+public interface CompanyService {
+    ApiResult<List<CompanyDTO>> getAllCompanyInfo();
+    ApiResult<CompanyDTO> getOneCompany(long id);
+    ApiResult<?> deleteCompanyById(long id);
+    ApiResult<?> saveCo0mpany(CompanyDTO companyDTO);
+    ApiResult<?> changeCompany(CompanyDTO companyDTO,long id);
+}
