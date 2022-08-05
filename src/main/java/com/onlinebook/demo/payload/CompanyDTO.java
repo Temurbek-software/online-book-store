e54131aa8245694b6f4f03315ac76a4b46af5d0b
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-08-06 00:24:45 +0500

    Create CompanyDTO.java

---
+++ src/main/java/com/onlinebook/demo/payload/CompanyDTO.java
@@ -0,0 +1,26 @@
+package com.onlinebook.demo.payload;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.onlinebook.demo.entity.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.Set;
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CompanyDTO {
+    private Long id;
+    private String nameOfCompany;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private Date yearOfPublished;
+    private String description;
+    private Set<Product> productSet;
+}
