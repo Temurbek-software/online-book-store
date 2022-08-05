7a5b608875853fe08a2606f8297f22d9625e310d
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-08-06 00:25:14 +0500

    Update CompanyController.java

--- src/main/java/com/onlinebook/demo/controller/CompanyController.java
+++ src/main/java/com/onlinebook/demo/controller/CompanyController.java
@@ -1,12 +1,38 @@
 package com.onlinebook.demo.controller;
 
+import com.onlinebook.demo.payload.ApiResult;
+import com.onlinebook.demo.payload.CompanyDTO;
+import com.onlinebook.demo.payload.ProductDTO;
+import com.onlinebook.demo.service.company.CompanyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/company")
-public class CompanyController {
+public class CompanyController
+{
+    private final CompanyService companyService;
 
+    @GetMapping("/all")
+    public ApiResult<?> getAllCompany()
+    {
+        return companyService.getAllCompanyInfo();
+    }
+
+    @PostMapping("/save")
+    public ApiResult<String> saveCompany(CompanyDTO companyDTO) {
+        return (ApiResult<String>) companyService.saveCo0mpany(companyDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ApiResult<CompanyDTO> getOneCompany(@PathVariable long id) {
+        return companyService.getOneCompany(id);
+    }
+
+    @PutMapping("/{id}")
+    public ApiResult<?> updateCompany(@PathVariable long id, @RequestBody CompanyDTO companyDTO)
+    {
+       return  companyService.changeCompany(companyDTO, id);
+    }
 }
