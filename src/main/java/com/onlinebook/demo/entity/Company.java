b4714af9de88bbb573634bd21f3f83d13334ea81
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-08-06 00:24:49 +0500

    Create Company.java

---
+++ src/main/java/com/onlinebook/demo/entity/Company.java
@@ -0,0 +1,51 @@
+package com.onlinebook.demo.entity;
+
+
+import com.onlinebook.demo.entity.template.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CollectionId;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "company")
+public class Company extends BaseEntity
+{
+   @Column(name ="nameOfCompany")
+   @NotNull
+   private String nameOfCompany;
+
+   @Column(name = "address")
+   @NotNull
+   private String address;
+
+   @Column(name = "phoneNumber")
+   @NotNull
+   private String phoneNumber;
+
+   @Column(name = "email")
+   @NotNull
+   private String email;
+
+   @Column(name = "yearOfPublished")
+   @NotNull
+   private Date yearOfPublished;
+
+   @Column(name = "description")
+   @NotNull
+   private String description;
+
+   @OneToMany(mappedBy = "company")
+   private Set<Product> product_Id=new HashSet<>();
+}
