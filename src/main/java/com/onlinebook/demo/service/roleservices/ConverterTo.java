da4ccc48c3082ea8e5ce987f1ab0026910f59346
Temurbek-software <temurbek.zaripov140996@mail.ru>
2022-08-06 00:25:10 +0500

    Update ConverterTo.java

--- src/main/java/com/onlinebook/demo/service/roleservices/ConverterTo.java
+++ src/main/java/com/onlinebook/demo/service/roleservices/ConverterTo.java
@@ -1,9 +1,11 @@
 package com.onlinebook.demo.service.roleservices;
 
 import com.onlinebook.demo.entity.Author;
+import com.onlinebook.demo.entity.Company;
 import com.onlinebook.demo.entity.Product;
 import com.onlinebook.demo.entity.Publisher;
 import com.onlinebook.demo.payload.AuthorDTO;
+import com.onlinebook.demo.payload.CompanyDTO;
 import com.onlinebook.demo.payload.ProductDTO;
 import com.onlinebook.demo.payload.PublisherDTO;
 
@@ -14,4 +16,6 @@ public interface ConverterTo {
     Author toAuhtor(AuthorDTO authorDTO);
     Publisher toPublisher(PublisherDTO publisherDTO);
     PublisherDTO toPublisherDTO(Publisher publisher);
+    Company toCompany(CompanyDTO companyDTO);
+    CompanyDTO COMPANY_DTO(Company company);
 }
