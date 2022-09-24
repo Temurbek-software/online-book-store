90d3779fcf9e53a30c9cf77704affc2148e0062f
Temurbek-software <67744075+Temurbek-software@users.noreply.github.com>
2022-09-24 13:49:03 +0500

    deleted mapToAuthor

--- src/main/java/com/onlinebook/demo/service/author/AuthorService.java
+++ src/main/java/com/onlinebook/demo/service/author/AuthorService.java
@@ -17,6 +17,6 @@ public interface AuthorService
     ApiResult<String> updatingAuthorById(Long id,AuthorDTO authorDTO);
 
     Author mapToAuthorDTO(AuthorDTO authorDTO);
-    AuthorDTO mapToAuthor(Author author);
+
 
 }
