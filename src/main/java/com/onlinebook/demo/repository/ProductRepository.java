cad73e75e434d63842a7a21f2ac33d80ceb187ab
Temurbek-software <67744075+Temurbek-software@users.noreply.github.com>
2022-09-27 09:40:26 +0500

    Update ProductRepository.java

--- src/main/java/com/onlinebook/demo/repository/ProductRepository.java
+++ src/main/java/com/onlinebook/demo/repository/ProductRepository.java
@@ -3,10 +3,12 @@ package com.onlinebook.demo.repository;
 import com.onlinebook.demo.entity.Product;
 import com.onlinebook.demo.payload.ProductDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -17,4 +19,8 @@ public interface ProductRepository extends JpaRepository<Product, Long>
 
     @Query(value = "select * from product s where s.is_deleted=:true", nativeQuery = true)
     List<ProductDTO> getAllProductsIfDeleted();
+    @Transactional
+    @Modifying
+    @Query(value = "",nativeQuery = true)
+    void insertNewProduct(@Param("newProduct") ProductDTO productDTO);
 }
